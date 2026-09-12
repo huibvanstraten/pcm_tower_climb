56d818552dccab1361cdcd01fc2ab832bab6d816
huibvanstraten <huib.vanstraten@cohesion.nl>
2026-09-12 17:43:33 +0200

    improvements on dynamic player creation

--- script/main.gd
+++ script/main.gd
@@ -1,13 +1,14 @@
 class_name Main
 extends Node
 
-
+@export var player_input_session_scene: PackedScene
 @export var availableLevels: Array[LevelData]
 
 @onready var level_container: Node = $LevelContainer
 @onready var player_container: Node = $PlayerContainer
 @onready var input_session_container: Node = $PlayerInputSessionContainer
 
+const MAX_PLAYERS := 4
 
 func _input(event: InputEvent) -> void:
 	var device := PlayerInputDevice.from_event(event)
@@ -45,19 +46,6 @@ func load_level(level_id: int) -> void:
 	LevelManager.load_level(level_id)
 
 	position_joined_players()
-
-
-func get_available_input_session() -> PlayerInputSession:
-	for child in input_session_container.get_children():
-		var session := child as PlayerInputSession
-
-		if session == null:
-			continue
-
-		if not session.is_joined():
-			return session
-
-	return null
 	
 	
 func activate_session_player(session: PlayerInputSession) -> void:
@@ -90,9 +78,6 @@ func is_device_assigned(device: PlayerInputDevice) -> bool:
 		if session == null:
 			continue
 
-		if not session.is_joined():
-			continue
-
 		if session.input_device.matches(device):
 			return true
 
@@ -100,14 +85,51 @@ func is_device_assigned(device: PlayerInputDevice) -> bool:
 	
 	
 func join_device(device: PlayerInputDevice) -> void:
-	var session := get_available_input_session()
+	var player_slot := get_available_player_slot()
 
-	if session == null:
-		print("JOIN REQUEST REJECTED: no available input session")
+	if player_slot == -1:
+		print("JOIN REQUEST REJECTED: no available player slot")
 		return
 
-	session.assign_device(device)
+	var session := create_input_session(
+		player_slot,
+		device
+	)
+
 	activate_session_player(session)
+
+	print(
+		"DEVICE JOINED: ",
+		PlayerInputDevice.Type.keys()[device.type],
+		" ",
+		device.device_id,
+		" -> player slot ",
+		player_slot
+	)
+	
+
+func create_input_session(
+	player_slot: int,
+	device: PlayerInputDevice
+) -> PlayerInputSession:
+	var session := player_input_session_scene.instantiate() as PlayerInputSession
+
+	session.player_slot = player_slot
+	session.name = "PlayerInputSession%s" % player_slot
+
+	input_session_container.add_child(session)
+
+	session.assign_device(device)
+
+	return session
+
+
+func get_available_player_slot() -> int:
+	for player_slot in range(1, MAX_PLAYERS + 1):
+		if not is_player_slot_used(player_slot):
+			return player_slot
+
+	return -1
 	
 	
 func position_joined_players() -> void:
@@ -135,3 +157,16 @@ func position_joined_players() -> void:
 			session.player_slot,
 			spawn_position
 		)
+	
+
+func is_player_slot_used(player_slot: int) -> bool:
+	for child in input_session_container.get_children():
+		var session := child as PlayerInputSession
+
+		if session == null:
+			continue
+
+		if session.player_slot == player_slot:
+			return true
+
+	return false
