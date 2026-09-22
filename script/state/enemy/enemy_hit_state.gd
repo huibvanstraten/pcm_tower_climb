ffb2354bec644b0b0544abe8079b3bfa7331bcd2
Michiel Oosterhuis <michiel.oosterhuis@alliander.com>
2026-09-22 23:16:43 +0200

    WIP

--- script/state/enemy/enemy_hit_state.gd
+++ script/state/enemy/enemy_hit_state.gd
@@ -2,19 +2,20 @@ class_name EnemyHitState
 extends EnemyState
 
 const NAME := "Hit"
-@export var physics_component: PhysicsComponent
-@export var move_component: MoveComponent
-@export var flip_component: FlipComponent
 var hit_animation_finished: bool = false
+@export var health_component: HealthComponent
+@export var hurt: HurtComponent
 
 func enter() -> void:
 	super()
 
 	var hit = entity.hit
+	health_component.take_hit(hit)
+	physics_component.apply_knockback(hit.direction, hit.knockback)
 	entity.hit = null
 
 	hit_animation_finished = false
-	animation_player.play(&"hit")
+	animation_player.play(&"enemy_hit")
 
 func exit() -> void:
 	super()
@@ -23,11 +24,13 @@ func physics_update(delta: float) -> String:
 	physics_component.apply_gravity(delta)
 
 	if hit_animation_finished:
-		return DieState.NAME
+		if health_component.health == 0:
+			return EnemyDieState.NAME
+		else:
+			return PatrolState.NAME
 
 	return "None"
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name != &"hit":
-		return
-	hit_animation_finished = true
+	if anim_name == &"enemy_hit":
+		hit_animation_finished = true
