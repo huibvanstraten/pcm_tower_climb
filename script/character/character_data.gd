b5d12ebd7b0a77cfa966d3c2db1be7f3c0c3ccfc
huibvanstraten <huib.vanstraten@cohesion.nl>
2026-09-24 08:51:08 +0200

    player instances have their own specs. choose different players from player select

---
+++ script/character/character_data.gd
@@ -0,0 +1,26 @@
+
+class_name CharacterData
+extends Resource
+
+@export_group("Identity")
+@export var id: StringName
+@export var display_name: String
+@export_multiline var description: String
+
+@export_group("Visuals")
+@export var portrait: Texture2D
+@export var sprite_frames: SpriteFrames
+
+@export_group("Ground Movement")
+@export var speed: float = 100.0
+@export var acceleration: float = 2000.0
+@export var friction: float = 2000.0
+
+@export_group("Air Movement")
+@export var air_speed: float = 150.0
+@export var air_acceleration: float = 1000.0
+@export var air_friction: float = 300.0
+
+@export_group("Jump")
+@export var jump_velocity: float = -400.0
+@export_range(0.0, 1.0) var jump_cut_multiplier: float = 0.4
