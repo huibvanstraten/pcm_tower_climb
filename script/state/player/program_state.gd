f52c59ce880739f9c798034956b601b4a1253389
Michiel Oosterhuis <michiel.oosterhuis@alliander.com>
2026-09-24 11:23:20 +0200

    Fix respawn

--- script/state/player/program_state.gd
+++ script/state/player/program_state.gd
@@ -1,11 +1,16 @@
 class_name ProgrammingState 
 extends PlayerState
+var programming_finished: bool = false
 
 const NAME := "Program"
+
+func _ready() -> void:
+	EventManager.programming_finished.connect(_finish_programming)
 
 func enter() -> void:
 	super()
 	move_component.accute_stop()
+	EventManager.programming_started.emit(entity)
 
 func exit() -> void:
 	super()
@@ -13,7 +18,15 @@ func exit() -> void:
 func physics_update(delta: float) -> String:
 	if entity.is_hit():
 		return HitState.NAME
-	elif entity.command.interact_pressed:
+	if programming_finished:
+		programming_finished = false
 		return IdleState.NAME
-	else:
-		return "None"
+	if entity.command.interact_pressed:
+		EventManager.programming_cancelled.emit(entity)
+		return IdleState.NAME
+	return "None"
+
+func _finish_programming(player: Player) -> void:
+	print("Finished programming received", player.name)
+	if player == entity:
+		programming_finished = true
